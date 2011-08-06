38d4cd562a5ab560d87cf0ded3c2fe934a257a2c
dipak.chirmade@gmail.com <dipak.chirmade@gmail.com@36075ba7-4f0e-b2ff-6407-d93fbf954068>
2011-08-06 14:00:49 +0000

    Configurations are now taken from file instead of previous hardcoded values. Will do merge with mini-rule-engine in r25/26.

---
+++ levee-mini-rule-engine/configuration-file.cpp
@@ -0,0 +1,172 @@
+//
+// 
+// Levee Health Monitoring And Surveillance System (Real-time)
+// Copyright (C) 2011 Fibercorps (http://www.fibercorps.com) 
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// GSoC11 Developer: Dipak 
+// GSoC11 Dashboard: http://www.google-melange.com/gsoc/project/google/gsoc2011/q1w2e3r4/12001
+// GSoC11 Mentor   : Dr.Raju Gottumukkala and Crawford Comeaux
+// 
+//
+// This will take configurations from file  
+//
+
+#ifndef CONFIGURATION_FILE_CPP
+#define CONFIGURATION_FILE_CPP
+
+// Include files for string and io operations 
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
+#include <cstdlib>
+#include <sstream>
+
+#include "configuration-file.hpp"
+
+using namespace std;
+
+//
+// Desc: Initializes few variables  
+// Arguments: Nothing, void  
+// Returns: Nothing, void 
+//  
+
+ConfigurationFile::ConfigurationFile( void ){
+
+    // Do initialization if any!
+    isDebugEnabled = true;
+    rawConfigurationFileBuffer.empty();     
+}
+
+//
+// Desc: Reset if any. Not in use as of now!
+// Arguments: Nothing, void  
+// Returns: Nothing, void 
+//  
+
+ConfigurationFile::~ConfigurationFile( void ){
+
+}
+
+//
+// Desc: Extracts values of valid given key 
+// Arguments: String, Key of which value need to be extracted.
+// Returns: String, Value of a valid given key 
+//  
+
+string ConfigurationFile::getValueOfGivenKey( string key ){
+   
+     string tValueString = ""; 
+     string tToken = "";
+      
+     // Process raw configuration file buffer to pullout key-values. 
+     istringstream iss( rawConfigurationFileBuffer );    
+     while( getline( iss, tToken, '\n' ) )
+     {
+       // Find the key and get the value 
+       if( tToken.rfind( key ) != string::npos ) {
+           
+          tValueString = tToken.substr( tToken.find_last_of( ":=" ) + 1 ); 
+       }
+     }
+
+     return tValueString;  
+}
+
+//
+// Desc: Reads configurations into raw string buffer file
+// Arguments: String, configuration file name.
+// Returns: bool, True if all is good otherwise false
+//  
+
+bool ConfigurationFile::readConfigurationFile( string fileName = "configuration.conf" ){
+ 
+      bool tReturn = false;
+      string tLineStringBuffer = "";  
+
+      // Open the configuration file and do reading 
+      ifstream fileHandle ( fileName.c_str() );
+      if( fileHandle.is_open() )
+      {
+       
+        // Flush the raw buffer
+        rawConfigurationFileBuffer.empty(); 
+
+        while ( fileHandle.good() )
+       {
+         getline ( fileHandle, tLineStringBuffer );
+         rawConfigurationFileBuffer += tLineStringBuffer + "\n";
+         tReturn = true; 
+       }
+         fileHandle.close();
+      }
+      else{
+           printDebugMessages( "Opps! Unable to open configuration file: " + fileName );
+           tReturn = false;    
+      } 
+      
+      return tReturn;       
+}
+
+//
+// Desc: This should print debug messages only if it is enabled.   
+// Arguments: String, Lines which needs to be printed on standard console. 
+// Returns: Nothing, void 
+//   
+ 
+void ConfigurationFile::printDebugMessages( string debugLines ){
+
+   // Get current time and stamp it with logs 
+   time_t tRawTime;
+   time ( &tRawTime );
+ 
+   string tLocaltime = asctime( localtime( &tRawTime ) );
+   tLocaltime.replace( tLocaltime.find( "\n" ), 1 ,"\0" ); // Replace \n at the end of time string
+  
+   // Print debug messages to standard console. 
+   if( isDebugEnabled )
+       cout << "<br>" << tLocaltime << " : " << debugLines << endl; 
+
+   return; 
+}
+
+#endif
+
+//
+// Desc: Main for configuration file   
+// Arguments: Nothing, void 
+// Returns: Nothing, void 
+//   
+
+#ifdef CONFIGURATION_FILE_MAIN 
+int main( void ){
+
+    ConfigurationFile configurationFile;  
+
+    if( !configurationFile.readConfigurationFile( "configuration.conf" ) ) 
+        return EXIT_FAILURE;
+    else{ 
+         // Get some demo key value pair...
+         configurationFile.printDebugMessages( "Value for Key 'ADXL335i-X-position' is " + 
+                                               configurationFile.getValueOfGivenKey( "ADXL335i-X-position" ) 
+                                             );
+    }
+
+    return EXIT_SUCCESS; 
+}
+#endif 
+
